13adabc99ed8a6af50c20df966aae17d52cd126b
Yiwei Zhang <zzyiwei@gmail.com>
2019-11-08 17:40:56 -0800

    Refactor the Engine codes into a class and fix the state restore logic

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,6 +1,5 @@
 #include <android/log.h>
 #include <android_native_app_glue.h>
-#include <jni.h>
 
 #include <mutex>
 
@@ -8,92 +7,105 @@
 
 #define ALOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "VKDEMO", __VA_ARGS__))
 
-struct State {
-    int32_t inputX;
-    int32_t inputY;
+class Engine {
+private:
+    struct State {
+        int32_t inputX;
+        int32_t inputY;
+        uint64_t frameCount;
 
-    State() : inputX(0), inputY(0) {}
+        State() : inputX(0), inputY(0), frameCount(0) {}
+    };
+
+public:
+    explicit Engine() : mIsAnimating(false) {}
+    bool isAnimating();
+    void drawFrame();
+    void onInitWindow(ANativeWindow* window, AAssetManager* assetManager);
+    void onLostFocus();
+    void onGainedFocus();
+    void onTermWindow();
+    void onSaveState(void** outSavedState, size_t* outSize);
+    void onStart(void* savedState);
+    int32_t onInputEvent(AInputEvent* event);
+
+private:
+    // mLock protects all members below
+    std::mutex mLock;
+    State mState;
+    VkHelper mVkHelper;
+    bool mIsAnimating;
+
+    // Engine constants
+    static constexpr const uint64_t kLogInterval = 100;
 };
 
-struct Engine {
-    android_app* app;
-
-    // this lock protects all fields below
-    std::mutex lock;
-    State state;
-    VkHelper vk;
-    bool animating;
-    uint64_t count;
-
-    Engine() : app(nullptr), lock(), state(), vk(), animating(false), count(0) {}
-};
-
-static bool engineIsAnimating(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
-    return engine->animating;
+bool Engine::isAnimating() {
+    std::lock_guard<std::mutex> lock(mLock);
+    return mIsAnimating;
 }
 
-static void engineInitDisplay(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
-    ALOGD("%s", __FUNCTION__);
-    engine->count = 0;
-    engine->vk.initialize(engine->app->window, engine->app->activity->assetManager);
-}
-
-static void engineDrawFrame(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
-    if (!engine->animating) {
+void Engine::drawFrame() {
+    std::lock_guard<std::mutex> lock(mLock);
+    if (!mIsAnimating) {
         return;
     }
 
-    engine->vk.drawFrame();
+    mVkHelper.drawFrame();
 
-    static constexpr const uint64_t kLogInterval = 100;
-    if (++engine->count % kLogInterval == 0) {
-        ALOGD("%s[%" PRIu64 "]", __FUNCTION__, engine->count);
+    if (++mState.frameCount % kLogInterval == 0) {
+        ALOGD("%s[%" PRIu64 "]", __FUNCTION__, mState.frameCount);
     }
 }
 
-static void enginePauseDisplay(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
+void Engine::onInitWindow(ANativeWindow* window, AAssetManager* assetManager) {
+    std::lock_guard<std::mutex> lock(mLock);
     ALOGD("%s", __FUNCTION__);
-    engine->animating = false;
+    mVkHelper.initialize(window, assetManager);
 }
 
-static void engineResumeDisplay(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
+void Engine::onLostFocus() {
+    std::lock_guard<std::mutex> lock(mLock);
     ALOGD("%s", __FUNCTION__);
-    engine->animating = true;
+    mIsAnimating = false;
 }
 
-static void engineTermDisplay(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
+void Engine::onGainedFocus() {
+    std::lock_guard<std::mutex> lock(mLock);
     ALOGD("%s", __FUNCTION__);
-    engine->animating = false;
-    engine->count = 0;
-    engine->vk.destroy();
+    mIsAnimating = true;
 }
 
-static void engineSaveState(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
+void Engine::onTermWindow() {
+    std::lock_guard<std::mutex> lock(mLock);
     ALOGD("%s", __FUNCTION__);
-    engine->app->savedState = malloc(sizeof(State));
-    *((State*)engine->app->savedState) = engine->state;
-    engine->app->savedStateSize = sizeof(State);
+    mIsAnimating = false;
+    mVkHelper.destroy();
 }
 
-static void engineRestoreState(Engine* engine) {
-    std::lock_guard<std::mutex> lock(engine->lock);
+void Engine::onSaveState(void** outSavedState, size_t* outSize) {
+    std::lock_guard<std::mutex> lock(mLock);
     ALOGD("%s", __FUNCTION__);
-    engine->state = *(State*)engine->app->savedState;
+    *outSize = sizeof(State);
+    *outSavedState = malloc(*outSize);
+    memcpy(*outSavedState, &mState, *outSize);
 }
 
-static int32_t engineUpdateState(Engine* engine, AInputEvent* event) {
-    std::lock_guard<std::mutex> lock(engine->lock);
+void Engine::onStart(void* savedState) {
+    std::lock_guard<std::mutex> lock(mLock);
     ALOGD("%s", __FUNCTION__);
+    if (savedState) {
+        memcpy(&mState, savedState, sizeof(State));
+        ALOGD("Restored from previous saved state");
+    }
+}
+
+int32_t Engine::onInputEvent(AInputEvent* event) {
+    std::lock_guard<std::mutex> lock(mLock);
     if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
-        engine->state.inputX = static_cast<int32_t>(AMotionEvent_getX(event, 0));
-        engine->state.inputY = static_cast<int32_t>(AMotionEvent_getY(event, 0));
+        mState.inputX = static_cast<int32_t>(AMotionEvent_getX(event, 0));
+        mState.inputY = static_cast<int32_t>(AMotionEvent_getY(event, 0));
+        ALOGD("AINPUT_EVENT_TYPE_MOTION X[%d] Y[%d]", mState.inputX, mState.inputY);
         return 1;
     }
     return 0;
@@ -101,47 +113,68 @@ static int32_t engineUpdateState(Engine* engine, AInputEvent* event) {
 
 static int32_t handleInputEvent(android_app* app, AInputEvent* event) {
     ALOGD("%s", __FUNCTION__);
-    return engineUpdateState(static_cast<Engine*>(app->userData), event);
+    return static_cast<Engine*>(app->userData)->onInputEvent(event);
 }
 
 static void handleAppCmd(android_app* app, int32_t cmd) {
     ALOGD("%s", __FUNCTION__);
     auto engine = static_cast<Engine*>(app->userData);
     switch (cmd) {
-        case APP_CMD_SAVE_STATE:
-            ALOGD("SAVE_STATE");
-            engineSaveState(engine);
+        case APP_CMD_INPUT_CHANGED:
+            ALOGD("APP_CMD_INPUT_CHANGED");
             break;
         case APP_CMD_INIT_WINDOW:
-            ALOGD("INIT_WINDOW");
-            engineInitDisplay(engine);
+            ALOGD("APP_CMD_INIT_WINDOW");
+            engine->onInitWindow(app->window, app->activity->assetManager);
             break;
         case APP_CMD_TERM_WINDOW:
-            ALOGD("TERM_WINDOW");
-            engineTermDisplay(engine);
-            break;
-        case APP_CMD_CONTENT_RECT_CHANGED:
-            ALOGD("RECT_CHANGED");
-            break;
-        case APP_CMD_CONFIG_CHANGED:
-            ALOGD("CONFIG_CHANGED");
+            ALOGD("APP_CMD_TERM_WINDOW");
+            engine->onTermWindow();
             break;
         case APP_CMD_WINDOW_RESIZED:
-            ALOGD("WINDOW_RESIZED");
-            break;
-        case APP_CMD_INPUT_CHANGED:
-            ALOGD("INPUT_CHANGED");
+            ALOGD("APP_CMD_WINDOW_RESIZED");
             break;
         case APP_CMD_WINDOW_REDRAW_NEEDED:
-            ALOGD("WINDOW_REDRAW_NEEDED");
+            ALOGD("APP_CMD_WINDOW_REDRAW_NEEDED");
+            break;
+        case APP_CMD_CONTENT_RECT_CHANGED:
+            ALOGD("APP_CMD_RECT_CHANGED");
             break;
         case APP_CMD_GAINED_FOCUS:
-            ALOGD("GAINED_FOCUS");
-            engineResumeDisplay(engine);
+            ALOGD("APP_CMD_GAINED_FOCUS");
+            engine->onGainedFocus();
             break;
         case APP_CMD_LOST_FOCUS:
-            ALOGD("LOST_FOCUS");
-            enginePauseDisplay(engine);
+            ALOGD("APP_CMD_LOST_FOCUS");
+            engine->onLostFocus();
+            break;
+        case APP_CMD_CONFIG_CHANGED:
+            ALOGD("APP_CMD_CONFIG_CHANGED");
+            break;
+        case APP_CMD_LOW_MEMORY:
+            ALOGD("APP_CMD_LOW_MEMORY");
+            break;
+        case APP_CMD_START:
+            ALOGD("APP_CMD_START");
+            if (app->savedState != nullptr) {
+                engine->onStart(app->savedState);
+            }
+            break;
+        case APP_CMD_RESUME:
+            ALOGD("APP_CMD_RESUME");
+            break;
+        case APP_CMD_SAVE_STATE:
+            ALOGD("APP_CMD_SAVE_STATE");
+            engine->onSaveState(&app->savedState, &app->savedStateSize);
+            break;
+        case APP_CMD_PAUSE:
+            ALOGD("APP_CMD_PAUSE");
+            break;
+        case APP_CMD_STOP:
+            ALOGD("APP_CMD_STOP");
+            break;
+        case APP_CMD_DESTROY:
+            ALOGD("APP_CMD_DESTROY");
             break;
         default:
             ALOGD("UNKNOWN CMD[%d]", cmd);
@@ -168,12 +201,6 @@ void android_main(android_app* app) {
     app->onInputEvent = handleInputEvent;
     app->activity->callbacks->onNativeWindowResized = handleNativeWindowResized;
 
-    engine.app = app;
-
-    if (app->savedState != nullptr) {
-        engineRestoreState(&engine);
-    }
-
     while (true) {
         int events;
         android_poll_source* source;
@@ -181,18 +208,19 @@ void android_main(android_app* app) {
         // If not animating, we will block forever waiting for events.
         // If animating, we loop until all events are read, then continue
         // to draw the next frame of animation.
-        while (ALooper_pollAll(engineIsAnimating(&engine) ? 0 : -1, nullptr, &events,
-                               (void**)&source) >= 0) {
+        while (ALooper_pollAll(engine.isAnimating() ? 0 : -1, nullptr, &events, (void**)&source) >=
+               0) {
             if (source != nullptr) {
                 source->process(app, source);
             }
 
             if (app->destroyRequested != 0) {
-                engineTermDisplay(&engine);
+                ALOGD("Destroy requested");
+                engine.onTermWindow();
                 return;
             }
         }
 
-        engineDrawFrame(&engine);
+        engine.drawFrame();
     }
 }
