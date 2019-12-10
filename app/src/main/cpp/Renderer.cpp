c3996fc267db9608a93c5e0a0b90d10ad6f76561
Yiwei Zhang <zzyiwei@gmail.com>
2019-12-10 14:33:08 -0800

    Remove redundant header

--- app/src/main/cpp/Renderer.cpp
+++ app/src/main/cpp/Renderer.cpp
@@ -1,7 +1,6 @@
 #include "Renderer.h"
 
 #include <glm/glm.hpp>
-#include <glm/gtc/constants.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #define STB_IMAGE_IMPLEMENTATION
